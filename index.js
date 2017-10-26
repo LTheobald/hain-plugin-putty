4e24d6eaef40a53b2ab8eb50b539551f3c6b05b9
Mikalai Kukhta <mikalai.kukhta@gmail.com>
2017-10-26 12:10:50 +0300

    - add search in stored PuTTY sessions - add PuTTY command launch item - add "reload" command

--- index.js
+++ index.js
@@ -5,26 +5,111 @@ module.exports = (pluginContext) => {
   const logger = pluginContext.logger;
   const exec = require('child_process').exec;
 
+  const registry_key = "HKEY_CURRENT_USER\\Software\\SimonTatham\\PuTTY\\Sessions";
+  var stored_sessions = [];
+
+  function loadPuttySessions() {
+    logger.log('loading Putty sessions...')
+
+    exec("reg query " + registry_key, (error, stdout, stderr) => {
+      
+      if (error) {
+        logger.log("putty reg error: " + stderr);
+        return;
+      }
+
+      stored_sessions = [];
+
+      stdout.split("\r\n").forEach((item) => {
+
+        if (item.trim().length === 0) return;
+
+        let session_name = decodeURI(item.slice(registry_key.length + 1));
+
+        if (session_name == "Default Settings") return;
+
+        stored_sessions.push(session_name);
+      });
+
+      stored_sessions.sort();
+
+      logger.log("putty sessions: " + stored_sessions);
+    });
+
+  }
+
   function startup() {
-    // you can do some preparations here
+    loadPuttySessions();
   }
 
   function search(query, res) {
-    // Will attempt to reference stored sessions at a later date. For now just return input as is
-    if (!query || query.trim().length === 0) {
-      return;
+    let found_matches = 0;
+    let has_query = false;
+
+    let filter_call = (item) => { return true; };
+
+    // search query
+    if (!(!query || query.trim().length === 0)) {
+      query = query.trim();
+      has_query = true;
+
+      filter_call = (item) => {
+        let found = item.toLowerCase().startsWith(query.toLowerCase()) ? true : false;
+        if (found) found_matches++;
+        return found;
+      }
     }
 
-    return res.add({
-      id: query,
-      title: query,
-      desc: 'SSH to '+ query,
-      icon: '#fa fa-chevron-right'
+    stored_sessions.forEach((item) => {
+      if (filter_call(item)) {
+        res.add({
+          id: 'session',
+          title: item,
+          payload: item,
+          desc: 'open PuTTY session \"' + item + '\"',
+          icon: '#fa fa-chevron-right'
+        });
+      }
     });
+
+    // add "putty call"
+    res.add({
+      id: 'q',
+      title: has_query ? 'putty.exe ' + query  : query,
+      payload: query,
+      desc: 'Run PuTTY command',
+      icon: '#fa fa-search'
+    });
+
+    // other commands
+    res.add({
+      id: 'reload',
+      title: 'reload',
+      desc: 'reload putty sessions',
+      icon: '#fa fa-refresh'
+    });
+
   }
 
   function execute(id, payload) {
-    exec("START putty "+ id);
+    
+    logger.log("id: " + id, "payload: " + payload);
+
+    switch (id) {
+      case 'q':
+        if (payload.trim().length > 0) {
+          exec('START putty ' + payload);
+        }
+      break;
+
+      case 'session':
+        exec('START putty -load \"' + payload + '\"');
+      break;
+
+      case 'reload':
+        loadPuttySessions();
+      break;
+    }
   }
 
   return { startup, search, execute };
